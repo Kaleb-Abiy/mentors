67697ec0215f722994472471e1a6156f692de378
kaleb <kalebabiy2012@gmail.com>
2023-08-04 22:35:12 +0300

    first commit

---
+++ accounts/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import CustomeUser
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+class CustomeUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password", "is_user", "is_mentor")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions", "is_user", "is_mentor"
+            )}
+        ),
+    )
+admin.site.register(CustomeUser, CustomeUserAdmin)
---
+++ accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import CustomeUserManager
+
+
+# Create your models here.
+
+
+class CustomeUser(AbstractUser):
+    is_user = models.BooleanField(default=False)
+    is_mentor = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+
+    objects = CustomeUserManager()
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    
