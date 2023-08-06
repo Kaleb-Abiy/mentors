3c86a0507d573a20032791ac15a0fab4a1068e71
kaleb <kalebabiy2012@gmail.com>
2023-08-06 21:29:49 +0300

    login api done and register started

---
+++ accounts/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import CustomeUser
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    pass
--- mentors/settings.py
+++ mentors/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 
@@ -102,6 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        
+    )
+}
 
 
 # Internationalization
