b6a180615573581f20b82933b7976454761baede
kaleb <kalebabiy2012@gmail.com>
2024-01-19 16:47:04 +0300

    db changed

--- mentors/settings.py
+++ mentors/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 import environ
 import os
 from datetime import timedelta
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,11 +97,14 @@ WSGI_APPLICATION = 'mentors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+db_pass = env('DB_PASSWORD')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        f'postgresql://postgres:{db_pass}@postgres.railway.internal:5432/railway',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
