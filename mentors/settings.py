09cda1ec2d83389128017a013a928edee9a8a527
kaleb <kalebabiy2012@gmail.com>
2024-02-03 16:39:27 +0300

    added deployed domain to cors list

--- mentors/settings.py
+++ mentors/settings.py
@@ -82,6 +82,7 @@ CORS_ALLOW_ALL_ORIGINS:True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5174",
+    "https://mentorsapp.netlify.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
